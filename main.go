e20976432b6bc967ebadea738950724723b0e2e5
solipsis <david@keepkey.com>
2017-11-14 14:52:13 -0700

    filewatcher that runs gulp on detected file change

--- main.go
+++ main.go
@@ -6,52 +6,156 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 )
 
+type cache struct {
+	sync.RWMutex
+	internal map[string]time.Time
+}
+
+type fileInfo struct {
+	path string
+	ts   time.Time
+}
+
+type op int32
+
+const (
+	Edit op = iota
+	Add
+	Remove
+)
+
+var ops = map[op]string{
+	Edit:   "Edit",
+	Add:    "Add",
+	Remove: "Remove",
+}
+
+func (o op) String() string {
+	if str, ok := ops[o]; ok {
+		return str
+	}
+	return "Uknown Op"
+}
+
+func newCache() *cache {
+	return &cache{
+		internal: make(map[string]time.Time),
+	}
+}
+
 func main() {
+
 	flag.Parse()
 	roots := flag.Args()
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
-	filenames := make(chan string)
+	info := make(chan fileInfo)
 	start := time.Now()
 	defer func() {
 		fmt.Printf("elapsed time: %v\n", time.Since(start))
 	}()
+	scanRoots(roots, info)
+	cache := newCache()
+	for file := range info {
+		cache.Lock()
+		//fmt.Printf("storing: %v with time %v\n", file.path, file.ts)
+		cache.internal[file.path] = file.ts
+		cache.Unlock()
+	}
+	fmt.Println("Existing files cached. Watching for changes...")
+	poll(cache, roots)
+}
+
+func scanRoots(roots []string, res chan<- fileInfo) {
 	var wg sync.WaitGroup
 	for _, root := range roots {
 		wg.Add(1)
-		go walkDir(root, &wg, filenames)
+		go walkDir(root, &wg, res)
 	}
+
 	go func() {
 		wg.Wait()
-		close(filenames)
+		close(res)
 	}()
-	/*
-		for name := range filenames {
-			//	fmt.Println(name)
-		}
-	*/
-	fmt.Println("vim-go")
+
 }
 
-func walkDir(dir string, wg *sync.WaitGroup, filenames chan<- string) {
+/*
+ticker := time.NewTicker(duration)
+<-ticker.C
+ticker.Stop
+*/
+
+// TODO GOROUTINE LEAK
+func poll(c *cache, roots []string) {
+	tick := time.Tick(1000 * time.Millisecond)
+	for range tick {
+		//	fmt.Println("tick")
+		if detectChanges(c, roots) {
+			gulp()
+		}
+	}
+}
+
+func detectChanges(c *cache, roots []string) (change bool) {
+	res := make(chan fileInfo)
+	scanRoots(roots, res)
+	for info := range res {
+		if c.internal[info.path] != info.ts {
+			fmt.Println("file change detected", info.path)
+			change = true
+			c.Lock()
+			c.internal[info.path] = info.ts
+			c.Unlock()
+		}
+	}
+
+	return change
+}
+
+func walkDir(dir string, wg *sync.WaitGroup, info chan<- fileInfo) {
 	defer wg.Done()
 	for _, entry := range dirents(dir) {
-		filenames <- entry.Name()
+		if strings.HasSuffix(entry.Name(), ".swp") {
+			continue
+		} // TODO make ignore behavior more robust
 		if entry.IsDir() {
 			wg.Add(1)
 			subdir := filepath.Join(dir, entry.Name())
-			go walkDir(subdir, wg, filenames)
+			go walkDir(subdir, wg, info)
+		} else {
+			info <- fileInfo{filepath.Join(dir, entry.Name()), entry.ModTime()}
 		}
+	}
+}
+
+// sema is a counting semaphore to limit concurrency in dirents
+// so we don't run out of file headers
+var sema = make(chan struct{}, 20)
+
+// TODO make command passable from command line
+func gulp() {
+	cmd := exec.Command("gulp", "build")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("error during gulp:%v\n", err)
 	}
 }
 
 func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}
+	defer func() { <-sema }()
 
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
